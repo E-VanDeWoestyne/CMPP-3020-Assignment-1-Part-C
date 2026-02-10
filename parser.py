9c63243544926ca9e8ebcb3e4e3f696f1396e9c2
Ethan Van De Woestyne <ethan.van@pm.me>
2026-02-09 18:17:43 -0700

    Implements the LARK parsing with comments for info. Will add more code examples.

--- parser.py
+++ parser.py
@@ -33,3 +33,36 @@ ebnf = r"""
     %import common.WS
     %ignore WS
 """
+
+def run_parser():
+    # Gets the filename from the user.
+    print("\nWelcome to the EBNF Parser tool for CMPP-3020 Assignment 1.\nThis tool will parse a file containing code written in a specific EBNF grammar and display the parse tree.")
+    print("Multiple code examples are provided. 'code1.txt' contains the matching pseudocode from Part B, while other files contain other variations for testing.")
+    filename = input("Enter the filename: ")
+
+    # Try to open the file and read its contents. If the file is not found, catch the exception and print an error message.
+    try:
+        with open(filename, 'r') as file:
+            user_code = file.read()
+            
+        # Defines a Lark parser using the EBNF grammer to run from 'program' starting point.
+        parser = Lark(ebnf, start='program')
+
+        try:
+            # Parses the code for correct grammer and then prints a parse tree if successful.
+            print(f"\nParsing file: {filename}\n")
+            print(user_code)
+            parse_tree = parser.parse(user_code)
+            print("\nParse successful and raised no exceptions.")
+
+            print(parse_tree.pretty())
+
+        except Exception as e:
+            print(f"Error parsing the code: {e}")
+
+    except FileNotFoundError:
+        print(f"File {filename} not found. Please check the filename and try again.")
+        return
+
+if __name__ == "__main__":
+    run_parser()
